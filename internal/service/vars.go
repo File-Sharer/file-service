17c7d7ad602a05079800e537d4530330cdc0f7bb
morf1lo <abdullinti@gmail.com>
2025-08-03 23:46:02 +0500

    service/vars.go minor improvement

--- internal/service/vars.go
+++ internal/service/vars.go
@@ -7,15 +7,15 @@ type level struct {
 
 var (
 	levelSpaceSizes = map[uint8]level{
-		1: level{
+		1: {
 			maxFileSize: 1_073_741_824, // 1 GB in bytes
 			maxSpaceSize: 8_589_934_592, // 8 GB in bytes
 		},
-		2: level{
+		2: {
 			maxFileSize: 8_589_934_592, // 8 GB in bytes
 			maxSpaceSize: 17_179_869_184, // 16 GB in bytes
 		},
-		3: level{
+		3: {
 			maxFileSize: 8_589_934_592, // 8 GB in bytes
 			maxSpaceSize: 34_359_738_368, // 32 GB in bytes
 		},
